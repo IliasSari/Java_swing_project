d57dbf035a79e5373c2c3c7a7b0ba339ef530725
IliasSari <sarigilias@gmail.com>
2025-12-14 21:05:03 +0200

    second commit

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
 
 public class Main {
 
@@ -17,16 +16,16 @@ public class Main {
 		label.setIcon(icon);
 		label.setVerticalAlignment(JLabel.BOTTOM);
 		label.setHorizontalAlignment(JLabel.RIGHT);
-		
+		label.setBounds(0, 0, 75, 75);
 		JPanel redPanel = new JPanel();
 		redPanel.setBackground(Color.red);
 		redPanel.setBounds(0, 0, 250, 250);
-		redPanel.setLayout(new BorderLayout());
+		redPanel.setLayout(null);
 
 		JPanel bluePanel = new JPanel();
 		bluePanel.setBackground(Color.blue);
 		bluePanel.setBounds(250, 0, 250, 250);
-		bluePanel.setLayout(new BorderLayout());
+		bluePanel.setLayout(null);
 		
 		JPanel greenPanel = new JPanel();
 		greenPanel.setBackground(Color.green);
